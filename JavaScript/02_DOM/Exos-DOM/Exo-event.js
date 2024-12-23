3bbd9b55775cecb80bf70b457768ec3265e2747a
hawa <hawakone831@gmail.com>
2024-12-23 15:03:04 +0100

    Exos js

--- JavaScript/02_DOM/Exos-DOM/Exo-event.js
+++ JavaScript/02_DOM/Exos-DOM/Exo-event.js
@@ -19,3 +19,22 @@ rectangle.addEventListener("mouseleave", () => {
 rectangle.addEventListener("dblclick", () => {
   rectangle.style.backgroundColor = "green";
 });
+
+// ********************************** Exo 2 ***********************************
+let buttoncercle = document.getElementById("toggle-cercle");
+let cercle = document.querySelector(".cercle");
+
+// pour afficher ou cacher le cercle
+buttoncercle.addEventListener("click", function () {
+  cercle.classList.toggle("hidden");
+});
+
+//  pour changer la couleur au survol 
+cercle.addEventListener("mouseover", function () {
+  cercle.style.backgroundColor = "orange";
+});
+
+// revenir à la couleur de départ grâce à  un double-clic
+cercle.addEventListener("dblclick", () => {
+  cercle.style.backgroundColor = "rgb(7, 72, 72)";
+});
